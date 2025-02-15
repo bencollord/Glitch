ac38976189a0edeb7363c9506302262d0cacef51
Ben <g.ben.collord@gmail.com>
2025-02-14 19:55:09 -0700

    Add indented string builder

---
+++ Glitch/Text/Indentation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glitch.Text
+{
+    public readonly record struct Indentation
+    {
+        public static readonly Indentation Empty = new Indentation(string.Empty);
+
+        public static readonly Indentation Spaces = new Indentation(' ', 4);
+
+        public static readonly Indentation HalfSpaces = new Indentation(' ', 2);
+        
+        public static readonly Indentation Tabs = new Indentation('\t', 1);
+
+        private readonly string text;
+        private readonly int level;
+
+        public Indentation(char c, int count)
+            : this(c, count, 0) { }
+
+        public Indentation(char c, int count, int level)
+            : this(new string(c, count), level) { }
+
+        public Indentation(string text)
+            : this(text, 0) { }
+
+        public Indentation(string text, int level)
+        {
+            this.text = text;
+            this.level = level;
+        }
+
+        public int Level
+        {
+            get => level;
+            init
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Cannot have indent less than zero");
+                }
+
+                level = value;
+            }
+        }
+
+        public Indentation Replace(string text)
+            => new Indentation(text, Level);
+
+        public override string ToString() => Enumerable.Repeat(text, Level).Join();
+
+        public static string operator +(Indentation indent, string text)
+            => indent.ToString() + text;
+
+        public static Indentation operator +(Indentation indent, int level)
+            => indent with { Level = indent.Level + level };
+
+        public static Indentation operator -(Indentation indent, int level)
+            => indent with { Level = indent.Level - level };
+
+        public static Indentation operator ++(Indentation indent)
+            => indent + 1;
+
+        public static Indentation operator --(Indentation indent)
+            => indent - 1;
+    }
+}
---
+++ Glitch/Text/IndentedStringBuilder.cs
@@ -0,0 +1,122 @@
+﻿using System.Text;
+
+namespace Glitch.Text
+{
+    public class IndentedStringBuilder
+    {
+        private StringBuilder inner;
+        private Indentation indentation;
+        private bool shouldIndent = false;
+
+        public IndentedStringBuilder()
+            : this(Indentation.Spaces) { }
+
+        public IndentedStringBuilder(Indentation indentation)
+            : this(new StringBuilder(), indentation) { }
+
+        public IndentedStringBuilder(string text)
+            : this(text, Indentation.Spaces) { }
+
+        public IndentedStringBuilder(string text, Indentation indentation)
+            : this(new StringBuilder(text), indentation) { }
+
+        public IndentedStringBuilder(StringBuilder inner)
+            : this(inner, Indentation.Spaces) { }
+
+        public IndentedStringBuilder(StringBuilder inner, Indentation indentation)
+        {
+            this.inner = inner;
+            this.indentation = indentation;
+        }
+
+        public char this[int index]
+        {
+            get => inner[index];
+            set => inner[index] = value;
+        }
+
+        public Indentation Indentation
+        {
+            get => indentation;
+            set => indentation = value;
+        }
+
+        public IndentedStringBuilder Append(string text)
+        {
+            if (shouldIndent)
+            {
+                inner.Append(indentation);
+                shouldIndent = false;
+            }
+
+            inner.Append(text);
+
+            return this;
+        }
+
+        public IndentedStringBuilder AppendLine()
+        {
+            inner.AppendLine();
+            shouldIndent = true;
+            return this;
+        }
+
+        public IndentedStringBuilder AppendLine(string text)
+            => Append(text).AppendLine();
+
+        public IndentedStringBuilder AppendIf(bool condition, string text)
+        {
+            if (condition)
+            {
+                Append(text);
+            }
+
+            return this;
+        }
+
+        public IndentedStringBuilder AppendLineIf(bool condition)
+        {
+            if (condition)
+            {
+                AppendLine();
+            }
+
+            return this;
+        }
+
+        public IndentedStringBuilder AppendLineIf(bool condition, string text)
+            => AppendIf(condition, text).AppendLineIf(condition);
+
+        public IndentedStringBuilder Indent()
+        {
+            Indentation++;
+            return this;
+        }
+
+        public IndentedStringBuilder Unindent()
+        {
+            Indentation--;
+            return this;
+        }
+
+        public override string ToString() => inner.ToString();
+
+        public IDisposable BeginBlock() => new IndentationBlock(this);
+
+        private class IndentationBlock : IDisposable
+        {
+            private IndentedStringBuilder inner;
+
+            public IndentationBlock(IndentedStringBuilder inner)
+            {
+                this.inner = inner;
+                inner.Indentation++;
+            }
+
+            public void Dispose()
+            {
+                inner.Indentation--;
+            }
+        }
+    }
+}
