91d7f61c305287a401cb23d670ea3e3e428bacdb
Ben <g.ben.collord@gmail.com>
2025-11-16 09:44:38 -0700

    ToString override

--- Glitch.CodeAnalysis/CSharpFile.cs
+++ Glitch.CodeAnalysis/CSharpFile.cs
@@ -1,9 +1,8 @@
-﻿using Glitch.Functional.Results;
-using Glitch.IO;
+﻿using Glitch.IO;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Glitch.CodeAnalysis
 {
@@ -30,59 +29,234 @@ namespace Glitch.CodeAnalysis
                 .Compose(NamespaceInRootPrism)
                 .Compose(TypeInNamespacePrism);
 
-        private FileInfo file;
-        private Lazy<SyntaxTree> tree;
-        private Option<SyntaxTree> updated = default;
+        private State state;
 
-        public CSharpFile(string path)
-            : this(new FileInfo(path)) { }
+        public CSharpFile(FilePath path)
+            : this(new UnloadedFileState(path)) { }
 
         public CSharpFile(FileInfo file)
+            : this(new UnloadedFileState(file)) { }
+
+        private CSharpFile(State state)
         {
-            this.file = file;
-            tree = new(() => CSharpSyntaxTree.ParseText(file.ReadAllText()));
+            this.state = state;
         }
 
-        public FileInfo File => file;
+        public FilePath Path => state.Path;
 
-        public static CSharpFile Load(string path) => new(path);
+        public FileInfo File => new FileInfo(Path);
+
+        public static CSharpFile Load(FilePath path) => new(path);
 
         public static CSharpFile Load(FileInfo file) => new(file);
 
-        public SyntaxTree GetTree() => updated | tree.Value;
+        public static CSharpFile Create(FilePath path, SyntaxNode root) => new(new UnsavedFileState(path, root));
 
-        public CompilationUnitSyntax GetRoot() => GetTree().GetCompilationUnitRoot();
+        public static CSharpFile Create(FileInfo file, SyntaxNode root) => Create(file.FullName, root);
 
-        public CSharpFile Rewrite(CSharpSyntaxRewriter visitor)
-            => ReplaceRoot(visitor.Visit);
-
-        public CSharpFile ReplaceRoot(Func<SyntaxNode, SyntaxNode> updater)
+        public CompilationUnitSyntax GetRoot()
         {
-            return ReplaceRoot(updater(GetRoot()));
+            SetState(state.LoadTree(out var tree));
+
+            return tree.GetCompilationUnitRoot();
         }
 
-        public CSharpFile ReplaceRoot(SyntaxNode newRoot)
+        public CSharpFile CopyTo(FilePath path) => WithState(state.CopyTo(path));
+
+        public CSharpFile Rewrite(CSharpSyntaxRewriter visitor) => Rewrite(visitor.Visit);
+
+        public CSharpFile Rewrite(Func<SyntaxNode, SyntaxNode> updater) => SetState(state.Rewrite(updater));
+
+        public CSharpFile ReplaceRoot(SyntaxNode newRoot) => SetState(state.WithRoot(newRoot));
+
+        public CSharpFile Revert() => SetState(state.Revert());
+
+        public Unit SaveChanges() => SetState(state.SaveChanges()).Ignore();
+
+        public override string ToString() => state.ToString();
+
+        private CSharpFile SetState(State state)
         {
-            updated = tree.Value.WithRootAndOptions(newRoot, tree.Value.Options);
+            this.state = state;
             return this;
         }
 
-        public CSharpFile ReplaceTree(CSharpSyntaxTree newTree)
+        private CSharpFile WithState(State state) => new(state);
+
+        private abstract class State
         {
-            updated = newTree;
-            return this;
+            public abstract FilePath Path { get; }
+
+            public abstract State CopyTo(FilePath path);
+
+            public abstract State LoadTree(out SyntaxTree tree);
+
+            public State Rewrite(CSharpSyntaxRewriter visitor)
+                => Rewrite(visitor.Visit);
+
+            public virtual State WithRoot(SyntaxNode newRoot)
+                => Rewrite(_ => newRoot);
+
+            public abstract State Rewrite(Func<SyntaxNode, SyntaxNode> updater);
+
+            public abstract State Revert();
+
+            public abstract State SaveChanges();
+
+            public abstract override string ToString();
         }
 
-        public CSharpFile Revert()
+        private class UnloadedFileState : State
         {
-            updated = None;
-            return this;
+            private FilePath path;
+
+            public UnloadedFileState(FilePath path)
+            {
+                this.path = path;
+            }
+
+            public override FilePath Path => path;
+
+            public override State CopyTo(FilePath path) => new UnloadedFileState(path);
+
+            public override State LoadTree(out SyntaxTree tree)
+            {
+                var next = Load();
+
+                tree = next.Tree;
+
+                return next;
+            }
+
+            public override State Revert()
+            {
+                return this;
+            }
+
+            public override State Rewrite(Func<SyntaxNode, SyntaxNode> updater)
+            {
+                return Load().Rewrite(updater);
+            }
+
+            public override State SaveChanges()
+            {
+                return this;
+            }
+
+            public override string ToString() => $"Unloaded: {Path}";
+
+            private LoadedState Load()
+            {
+                using var stream = new FileStream(path, FileMode.Open);
+
+                var text = SourceText.From(stream);
+                var tree = CSharpSyntaxTree.ParseText(text);
+
+                return new LoadedState(path, tree);
+            }
         }
 
-        public void SaveChanges()
+        private class LoadedState : State
         {
-            using var stream = file.CreateText();
-            GetRoot().WriteTo(stream);
+            public LoadedState(FilePath path, SyntaxTree tree)
+            {
+                Path = path;
+                Tree = tree;
+            }
+
+            public override FilePath Path { get; }
+
+            public SyntaxTree Tree { get; }
+
+            public override State CopyTo(FilePath path) => new LoadedState(path, Tree);
+
+            public override State LoadTree(out SyntaxTree tree)
+            {
+                tree = Tree;
+                return this;
+            }
+
+            public override State Revert()
+            {
+                return this;
+            }
+
+            public override State Rewrite(Func<SyntaxNode, SyntaxNode> updater)
+            {
+                var root = Tree.GetRoot();
+                return new ModifiedState(Path, root, updater(root));
+            }
+
+            public override string ToString() => $"Loaded: {Path}";
+
+            public override State SaveChanges()
+            {
+                return this;
+            }
+        }
+
+        private class UnsavedFileState : State
+        {
+            public UnsavedFileState(FilePath path, SyntaxNode updated)
+            {
+                Path = path;
+                Updated = updated;
+            }
+
+            public override FilePath Path { get; }
+
+            public SyntaxNode Updated { get; }
+
+            public override State CopyTo(FilePath path) => new UnsavedFileState(path, Updated);
+
+            public override State LoadTree(out SyntaxTree tree)
+            {
+                tree = CreateTree(Updated);
+                return this;
+            }
+
+            public override State Revert() => this;
+
+            public override State Rewrite(Func<SyntaxNode, SyntaxNode> updater)
+            {
+                return new ModifiedState(Path, Updated, updater(Updated));
+            }
+
+            public override State SaveChanges()
+            {
+                using var stream = new StreamWriter(Path);
+
+                Updated.WriteTo(stream);
+
+                return new LoadedState(Path, CreateTree(Updated));
+            }
+
+            public override string ToString() => $"Unsaved: {Path}";
+
+            protected SyntaxTree CreateTree(SyntaxNode root) => CSharpSyntaxTree.Create((CSharpSyntaxNode)root, path: Path);
+        }
+
+        private class ModifiedState : UnsavedFileState
+        {
+            public ModifiedState(FilePath path, SyntaxNode original, SyntaxNode updated)
+                : base(path, updated)
+            {
+                Original = original;
+            }
+
+            public SyntaxNode Original { get; }
+
+            public override State Revert()
+            {
+                return new LoadedState(Path, CreateTree(Original));
+            }
+
+            public override string ToString() => $"Modified: {Path}";
+
+            public override State Rewrite(Func<SyntaxNode, SyntaxNode> updater)
+            {
+                return new ModifiedState(Path, Original, updater(Updated));
+            }
         }
     }
 }
